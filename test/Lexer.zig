4d8489b14e61a0d0398b6a06a5440d5daa2b57b0
amsitlab <dezavue3@gmail.com>
2025-08-23 00:26:14 +0700

    update: (test step) add another test case

--- test/Lexer.zig
+++ test/Lexer.zig
@@ -10,7 +10,32 @@ test "line comment" {
         .buf = "//this is line comment",
     };
     const token = lexer.next();
-    try testing.expect(token.next() == Tag.eof);
+    try testing.expect(token.tag == Tag.eof);
     try testing.expectEqual(token.loc.beg, lexer.buf.len);
+    try testing.expectEqual(token.loc.end, lexer.buf.len);
+}
+
+test "line comment with close" {
+    const lexer: Lexer = .{
+        .with_doc = false,
+        .buf = "//this is line comment*/",
+    };
+    const token = lexer.next();
+    try testing.expect(token.tag == Tag.eof);
+    try testing.expectEqual(token.loc.beg, lexer.buf.len);
+    try testing.expectEqual(token.loc.end, lexer.buf.len);
+}
+
+test "line comment with close then illegal" {
+    const lexer: Lexer = .{
+        .with_doc = false,
+        .buf = "//this is line comment*/ illegal",
+    };
+    var token = lexer.next();
+    try testing.expect(token.tag == Tag.illegal);
+    const loc = token.loc;
+    token = lexer.next();
+    try testing.expect(token.tag == Tag.eof);
+    try testing.expectEqual(loc.end, token.loc.beg);
     try testing.expectEqual(token.loc.end, lexer.buf.len);
 }
