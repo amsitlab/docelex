5fdec170a663c45f28a7f24fcaac5868671c7880
amsitlab <dezavue3@gmail.com>
2025-08-22 23:57:59 +0700

    fix: (test step) undefined semicolon

--- test/Lexer.zig
+++ test/Lexer.zig
@@ -7,7 +7,7 @@ const testig = @import("std").testing;
 test "line comment" {
     const lexer: Lexer = .{
         .with_doc = false,
-        .buf = "//this is line comment";
+        .buf = "//this is line comment",
     };
 
     try testing.expect(token.next() == Tag.eof);
