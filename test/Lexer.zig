65ceda1e0971f326543e622e791d64fe60690b7a
amsitlab <dezavue3@gmail.com>
2025-08-24 00:53:32 +0700

    text number literal (must be error)

--- test/Lexer.zig
+++ test/Lexer.zig
@@ -74,7 +74,12 @@ test "hex number" {
 test "invalid number" {
     const lexer = lex("1e");
     try testing.expectEqual(lexer.next().tag, Tag.Illegal);
-    try testing.expectEqual(lexer.next().tag, Tag.illegal);
+    const token = lexer.next();
+    std.debug.print("\nToken{{ .tag = {?}, .loc = {{ .beg = {d}, .end = {d} }} lex: {s}\n", .{
+        token.tag, token.loc.beg, token.loc.end,
+        lexer.buf[token.loc.beg..token.loc.end]
+    })
+    try testing.expectEqual(token.tag, Tag.illegal);
 }
 
 
