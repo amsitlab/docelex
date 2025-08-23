945bc5410ee540f1332bfcc3c2fbb8fd925a2381
amsitlab <dezavue3@gmail.com>
2025-08-24 01:48:48 +0700

    text number literal (must be error)

--- test/main.zig
+++ test/main.zig
@@ -1,6 +1,6 @@
 
 
 const _ = @import("./Lexer.zig");
-test "must be error" {
-    try @import("std").testing.expectEqual(true, false);
-}
+// test "must be error" {
+//     try @import("std").testing.expectEqual(true, false);
+// }
