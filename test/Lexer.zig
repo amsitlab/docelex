5df5d0e5107a1b8b9454f450887673ed7a792c25
amsitlab <dezavue3@gmail.com>
2025-08-26 00:38:30 +0700

    feat: add validator for number literal

--- test/Lexer.zig
+++ test/Lexer.zig
@@ -126,7 +126,7 @@ test "invalid number" {
             .trailing_underscore,
             .invalid_character,
             .invalid_exponent_sign,
-            .period_after_exponent
+            .period_after_exponent,
             => |i| std.debug.print("Malformed Number: at {i}", .{e});
         }
     }
